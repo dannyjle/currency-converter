725d6f4df2a2b0434e92c916e9d75c7bcb2063f3
Danny Le <le.dannyj@gmail.com>
2021-06-02 15:42:33 -0400

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function App() {
-  return <div>Hello, World!</div>
+  const [currency, setCurrency] = useState({ rates: [] })
+  const [amount, setAmount] = useState(1)
+
+  return (
+    <>
+      <header>
+        <h1>$Exchange Rate Calculator$</h1>
+      </header>
+      <main>
+        <div className="exchange">
+          <form>
+            <input type="number" placeholder="0.00" />
+          </form>
+          <button>Calculate</button>
+        </div>
+        <ul>
+          <li>USA</li>
+        </ul>
+      </main>
+    </>
+  )
 }
